cc2932dce903c48b069305bb4f47796f5820d94b
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-07-27 19:15:05 -0300

    Registration screen

--- src/screens/auth/register/index.js
+++ src/screens/auth/register/index.js
@@ -1,7 +1,39 @@
-const RegisterScreen = () => (
-    <>
-      Hello World
-    </>
-  );
+import React from 'react';
+import LogoImage from '../../../assets/images/logo.png';
+import { Column, Section, Title, Container, Card } from "rbx";
+import "../../../styles/auth.scss";
+import Header from './../../../components/header/index';
 
-  export default RegisterScreen;
+const RegisterScreen = () => (
+  <>
+    <Header className="custom-header" />
+    <Section size="medium" className="auth">
+      <Container>
+        <Column.Group centered className="is-vcentered">
+          <Column size={4}>
+            <Card className="custom-card">
+              <Card.Content>
+                <Section>
+                  <Column.Group centered>
+                    <Column size={12} className="has-text-centered">
+                      <img src={LogoImage} alt="Logo" />
+                    </Column>
+                  </Column.Group>
+                  <Column.Group centered>
+                    <Column size={12} className="has-text-centered">
+                      <Title size={6} className="has-text-grey">
+                        Your notes on the cloud
+                      </Title>
+                    </Column>
+                  </Column.Group>
+                </Section>
+              </Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+      </Container>
+    </Section>
+  </>
+);
+
+export default RegisterScreen;
