2f6887b920141bcda24e648bf53024f0a4257ed8
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-07-29 21:29:34 -0300

    added login Form

---
+++ src/components/auth/login_form/index.js
@@ -0,0 +1,79 @@
+import React, { Fragment, useState } from 'react';
+import { Button, Field, Control, Input, Column, Section, Help, Label } from "rbx";
+import { Navigate } from "react-router-dom";
+import UserService from "../../../services/users"
+
+function LoginForm() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [RedirectToRegister, setRedirectToRegister] = useState(false);
+  const [RedirectToNotes, setRedirectToNotes] = useState(false);
+  const [Error, setError] = useState(false);
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    try {
+      const user = await UserService.login({ email: email, password: password });
+      setRedirectToNotes(true);
+    } catch (error) {
+      setError(true)
+    }
+  }
+
+  if (RedirectToRegister)
+    return <Navigate to={{ pathname: "/register" }} />
+  else if (RedirectToNotes == true)
+    return <Navigate to={{ pathname: "/notes" }} />
+
+  return (
+    <Fragment>
+      <Column.Group centered>
+        <form onSubmit={handleSubmit}>
+          <Column size={12}>
+            <Field>
+              <Label size="small">Email:</Label>
+              <Control>
+                <Input
+                  type="email"
+                  required
+                  name="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Label size="small">Password:</Label>
+              <Control>
+                <Input
+                  type="password"
+                  required
+                  name="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </Control>
+            </Field>
+            <Field>
+              <Control>
+                <Column.Group breakpoint="mobile">
+                  <Column>
+                    <a onClick={e => setRedirectToRegister(true)} className="button is-white has-text-custom-purple">Register or</a>
+                  </Column>
+                  <Column>
+                    <Button color="custom-purple" outlined>Login</Button>
+                  </Column>
+                </Column.Group>
+              </Control>
+            </Field>
+            {Error && <Help color="danger">Email or Password invalid</Help>}
+          </Column>
+        </form>
+      </Column.Group>
+    </Fragment>
+  )
+}
+
+export default LoginForm;
