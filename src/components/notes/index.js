b320868700eeae56ece6fcdd4294ba1bf180b549
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-08-07 12:03:27 -0300

    Listing the notes

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,37 +1,55 @@
-
 import React, { Fragment, useEffect, useState } from 'react';
-import { Column, Button } from "rbx";
+import { Column } from "rbx";
 import "../../styles/notes.scss";
-import { push as Menu } from 'react-burger-menu'
-
+import { push as Menu } from 'react-burger-menu';
+import List from "../notes/list/index";
+import NoteService from '../../services/notes';
 
 function Notes(props) {
+  const [notes, setNotes] = useState([]);
+  const [currentNote, setCurrentNote] = useState({ title: "", body: "", id: "" });
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const response = await NoteService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse());
+      setCurrentNote(response.data[0]);
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id === id;
+    });
+    setCurrentNote(note);
+  }
 
   return (
     <Fragment>
-      <Column.Group className="notes" id="notes">
-        <Menu
-          pageWrapId={"notes-editor"}
+      <div className="notes" id="notes">
+        <Menu 
+          pageWrapId={"notes-editor"} 
           isOpen={props.isOpen}
           onStateChange={(state) => props.setIsOpen(state.isOpen)}
-          disableAutoFocus
+          disableAutoFocus 
           outerContainerId={"notes"}
           customBurgerIcon={false}
           customCrossIcon={false}
         >
-          <Column.Group>
-            <Column size={10} offset={1}>
-              Search...
-            </Column>
-          </Column.Group>
-          <p>List...</p>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={currentNote} />
         </Menu>
-
 
         <Column size={12} className="notes-editor" id="notes-editor">
           Editor...
         </Column>
-      </Column.Group>
+      </div>
     </Fragment>
   )
 }
---
+++ src/services/notes.js
@@ -0,0 +1,10 @@
+import Api from './api';
+
+
+const NotesService = {
+    index: () => Api.get('/notes',{
+        headers: {'x-acess-token': localStorage.getItem('token')}
+    })
+}
+
+export default NotesService
