054ff2550b1d0af2116f9ac1169725dc1caecf99
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-08-10 22:12:54 -0300

    styling registration page

--- src/components/auth/register_form/index.js
+++ src/components/auth/register_form/index.js
@@ -26,71 +26,69 @@ function RegisterForm() {
   }
 
   return (
-    <>
-      <Column.Group centered>
-        <form onSubmit={handleSubmit}>
-          <Column size={12}>
-            <Field>
-              <Label size="small" className="custom-label">Name:</Label>
-              <Control>
-                <Input
-                  type="text"
-                  value={name}
-                  onChange={e => setName(e.target.value)}
-                  required
-                  name="name"
-                />
-              </Control>
-            </Field>
-            <Field>
-              <Label size="small" className="custom-label">Email:</Label>
-              <Control>
-                <Input
-                  type="email"
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                  required
-                  name="email"
-                />
-              </Control>
-            </Field>
-            <Field>
-              <Label size="small" className="custom-label">Password:</Label>
-              <Control>
-                <Input
-                  type="password"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  required
-                  name="password"
-                />
-              </Control>
-            </Field>
-            <Field>
-              <Control>
-                <Column.Group>
-                  <Column>
-                    <button
-                      onClick={() => setRedirectToLogin(true)}
-                      className="button is-white has-text-custom-purple"
-                    >
-                      Login or
-                    </button>
-                  </Column>
+    <Column.Group className="column-group-centered">
+      <form onSubmit={handleSubmit} className="custom-register-form-r">
+        <Column size={12}>
+          <Field>
+            <Label size="small" className="custom-register-label-r">Name:</Label>
+            <Control>
+              <Input
+                type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                required
+                name="name"
+                className="custom-register-input-r"
+              />
+            </Control>
+          </Field>
+          <Field>
+            <Label size="small" className="custom-register-label-r">Email:</Label>
+            <Control>
+              <Input
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+                name="email"
+                className="custom-register-input-r"
+              />
+            </Control>
+          </Field>
+          <Field>
+            <Label size="small" className="custom-register-label-r">Password:</Label>
+            <Control>
+              <Input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required
+                name="password"
+                className="custom-register-input-r"
+              />
+            </Control>
+          </Field>
 
-                  <Column>
-                    <Button color="custom-purple" outlined>
-                      Register
-                    </Button>
-                  </Column>
-                </Column.Group>
-              </Control>
-            </Field>
-            {error && <Help color="danger">Email or Password invalid</Help>}
-          </Column>
-        </form>
-      </Column.Group>
-    </>
+          <Field>
+  <Control>
+    <div className="custom-register-buttons-group-r">
+      <button
+        onClick={() => setRedirectToLogin(true)}
+        className="register-button-r"
+      >
+        Login
+      </button>
+      <Button color="custom-purple" outlined className="register-button-r">
+        Register
+      </Button>
+    </div>
+  </Control>
+</Field>
+
+          {error && <Help color="danger" className="custom-help-message-r">Email or Password invalid</Help>}
+        </Column>
+      </form>
+    </Column.Group>
   );
 }
 
