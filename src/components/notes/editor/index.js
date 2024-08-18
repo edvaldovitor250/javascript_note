28dd1d9143fd1e345f35b937a6cd3ee3a44c0f0d
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-08-17 22:10:02 -0300

    add: edit color letter

--- src/components/notes/editor/index.js
+++ src/components/notes/editor/index.js
@@ -1,39 +1,43 @@
-
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useRef } from 'react';
 import ReactQuill from 'react-quill'; 
 import 'react-quill/dist/quill.snow.css'; 
+import '../../../styles/';  
 
 function Editor(props) {
-  const [currentContent, setCurrentContent] = useState('')
-  const [timer,setTimer] = useState(null);
+  const [currentContent, setCurrentContent] = useState('');
+  const [timer, setTimer] = useState(null);
+  const quillRef = useRef(null);
 
   const updateNote = (content) => {
     const title = content.replace(/(<([^>]+)>)/ig, "").slice(0, 30);
-    props.updateNote(props.note, {'title': title, 'body': content})
+    props.updateNote(props.note, { 'title': title, 'body': content });
   }
 
   const handleChange = (content, delta, source) => {
-    clearTimeout(timer)
-    if(source === 'user') {
-        setCurrentContent(content);
-        setTimer(setTimeout(() => updateNote(content),2000))
+    clearTimeout(timer);
+    if (source === 'user') {
+      setCurrentContent(content);
+      setTimer(setTimeout(() => updateNote(content), 2000));
     }
   }
 
+  useEffect(() => {
+    setCurrentContent(props.note.body);
+  }, [props.note]);
 
-  
-
-  useEffect(()=> {
-    setCurrentContent(props.note.body)
-  }, [props.note])
+  useEffect(() => {
+    if (quillRef.current) {
+      const editor = quillRef.current.getEditor();
+      editor.root.style.backgroundColor = '#6a0dad'; // Purple background
+      editor.root.style.color = '#ffffff'; // White text
+    }
+  }, [quillRef.current]);
 
   const modules = {
     toolbar: [
       [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
       ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, 
-       {'indent': '-1'}, {'indent': '+1'}],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
       ['link'],
       ['clean'],
     ]
@@ -41,8 +45,14 @@ function Editor(props) {
 
   return (
     <>
-<ReactQuill value={currentContent} onChange={handleChange} modules={modules}/>    </>
-  )
+      <ReactQuill
+        ref={quillRef}
+        value={currentContent}
+        onChange={handleChange}
+        modules={modules}
+      />
+    </>
+  );
 }
 
 export default Editor;
